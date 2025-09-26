e245b93399e1eef350eddd927493fff411bc7ebf
kainakamura <yamadeko2761@gmail.com>
2025-09-26 19:33:42 +0900

    ユーザー登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,14 +9,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,        keys: %i[nickname])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[nickname])
   end
-
-  private
-
-  def after_sign_in_path_for(resource)
-    root_path
-  end
-
-  def after_sign_out_path_for(resource_or_scope)
-    root_path
-  end
 end

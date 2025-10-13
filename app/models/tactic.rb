16766a2390bdd3e6af7898376a4fd376fdda46d5
kainakamura <yamadeko2761@gmail.com>
2025-10-13 16:54:11 +0900

    model修正

--- app/models/tactic.rb
+++ app/models/tactic.rb
@@ -3,5 +3,4 @@ class Tactic < ApplicationRecord
   has_many :videos, through: :video_tactics
   validates :title, presence: true
   validates :slug, presence: true, uniqueness: true
-  serialize :steps, JSON
 end
